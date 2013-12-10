81bd2e7ba7391131deedc7966552cf19aac6d264
Les Cordell <silvatici1066@gmail.com>
2013-12-09 22:50:21 -0800

    Update firewall.py

--- firewall.py
+++ firewall.py
@@ -3,20 +3,17 @@
 ##############################
 from blacksalt import BlackSalt
 iptables = BlackSalt()
-# When creating a BlackSalt instance you can pass in a dictionary
+# When creating a BlackSalt instance you can pass in keyword arguments
 # of arguments, the allowed arguments are:
-#   {
-#       "iptables": "BIN LOCATION HERE", // This defaults to /sbin/iptables
-#       "printmode": True or False,      // This defaults to True and will print the rules
-#                                        // to stdout when generating the tables.
-#       "scriptfile": "Name and location of file" // If set, generating rule will output
-#                                                 // to this file
-#   }
+#       iptables = "BIN LOCATION HERE" # This defaults to /sbin/iptables
+#       printmode = True or False     # This defaults to True and will print the rules
+#                                      # to stdout when generating the tables.
+#       scriptfile = "Name and location of file"  #If set, generating rule will output
+#                                                 # to this file
 # These can be set after creation, by just setting the variables i.e.
 # iptables.printmode = False
 # iptables.scriptfile = "/usr/local/scripts/firewall"
 # iptables.iptables = "/usr/local/bin/iptables"
-#
 
 
 SUBNETS = ["192.168.10.0/24", "192.168.11.0/24", "192.168.12.0/24"]
@@ -56,5 +53,7 @@ iptables.setrule(chain="input", interface={"direction": "in", "name": "eth0"}, d
 # Drop all NEW connections coming in
 iptables.setrule(chain="input", interface={"direction": "in", "name": "eth0"}, state="new", target="drop")
 
-# Generate the script
+# Generate the script, but first disable outputting to the console, and save it in a scriptfile
+iptables.printmode = False
+iptables.scriptfile = "firewall.sh"
 iptables.generate()
